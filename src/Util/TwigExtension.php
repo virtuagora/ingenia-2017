f86c78bb539f67ce0eebf2473009e42bcc7c37a7
Guillermo Croppi <guillermocroppi@gmail.com>
2017-04-07 19:12:20 -0300

    Cambios hecho por mi!

--- src/Util/TwigExtension.php
+++ src/Util/TwigExtension.php
@@ -38,6 +38,7 @@ class TwigExtension extends \Twig_Extension
             new \Twig_SimpleFunction('base_url', array($this, 'baseUrl')),
             new \Twig_SimpleFunction('is_current_path', array($this, 'isCurrentPath')),
             new \Twig_SimpleFunction('asset', array($this, 'asset')),
+            new \Twig_SimpleFunction('category_name', array($this, 'categoryName')),
         ];
     }
 
@@ -60,5 +61,26 @@ class TwigExtension extends \Twig_Extension
     public function asset($name)
     {
         return $this->helper->baseUrl().'/assets/'.$name;
+    }
+
+    public function categoryName($category){
+        if($category == 'social'){
+            return 'Integración Social';
+        }else if($category == 'educacion'){
+            return 'Educación';
+        }else if($category == 'comunicacion'){
+            return 'Comunicación';
+        }else if($category == 'ambiente'){
+            return 'Medio Ambiente';
+        }else if($category == 'cultura'){
+            return 'Cultura';
+        }else if($category == 'salud'){
+            return 'Salud';
+        }else if($category == 'deporte'){
+            return 'Deporte y recreación';
+        }else if($category == 'empleo'){
+            return 'Empleo y Capacitación';
+        }
+        return 'undefined';
     }
 }
